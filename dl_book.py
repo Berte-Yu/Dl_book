d41b534fc50b9fa4bde429de12f3e64db22a7172
Berte_yu <yubo_5698@163.com>
2019-10-17 17:26:42 +0800

    new add

---
+++ dl_book.py
@@ -0,0 +1,36 @@
+# coding:utf-8
+
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+
+def Get_book_url(page_html):
+    '''
+    通过html获取本页面所有书籍的url
+    '''
+    soup = BeautifulSoup(page_html)
+
+
+url = r'https://sobooks.cc/'
+
+# 打开浏览器
+dv = webdriver.Chrome()
+
+# 打开网页
+dv.get(url) 
+
+# 获取最大页数
+max_page = dv.find_element_by_css_selector("ul>li:last-child span").text
+
+max_page = int(max_page[1:-1])
+
+for i in range(max_page):
+    # 组装页
+    url = url + r'page/' + str(i+1)
+
+    dv.get(url) 
+    Get_book_url(dv.page_source)
+
+dv.close()
+    
+
